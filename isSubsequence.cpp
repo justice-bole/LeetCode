caca1715eb7734f183aa519b55a3eaa63f2c9436
NetworkerLurker <78961523+NetworkerLurker@users.noreply.github.com>
2022-11-01 17:51:26 +0100

    isSubsequence
    //Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
    //A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
    //of the characters without disturbing the relative positions of the remaining characters.
    //(i.e., "ace" is a subsequence of "abcde" while "aec" is not).

---
+++ isSubsequence.cpp
@@ -0,0 +1,22 @@
+// Created on 11/1/2022.
+//Is Subsequence
+//Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+//A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+//of the characters without disturbing the relative positions of the remaining characters.
+//(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+bool isSubsequence(std::string s, std::string t) {
+
+    int j{0};
+    for(int i{0}; i < t.length() and j < s.length(); ++i) {
+        if(s[j] == t[i]) {
+            ++j;
+        }
+    }
+    return (j == s.length());
+}
